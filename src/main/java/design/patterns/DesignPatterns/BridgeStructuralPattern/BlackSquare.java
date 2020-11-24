e572f1c52cb0d296f0ef424e5b6be481fca2319b
Muskan Salampuria <muski.kool@gmail.com>
2020-11-24 21:02:25 +0530

    Update BlackSquare.java

--- src/main/java/design/patterns/DesignPatterns/BridgeStructuralPattern/BlackSquare.java
+++ src/main/java/design/patterns/DesignPatterns/BridgeStructuralPattern/BlackSquare.java
@@ -1,7 +1,8 @@
 package design.patterns.DesignPatterns.BridgeStructuralPattern;
 
-public class BlackSquare implements DrawAPI{
+public class BlackSquare implements DrawAP
+  {
 	public void drawSquare(int side, int x, int y) {
 		System.out.println("Black Square with side: "+side+" , x: "+x+" ,y: "+y);
 	}
-}
+  }
